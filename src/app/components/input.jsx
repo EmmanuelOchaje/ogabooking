823444e2a124fc711887793403c93cb11d158e93
EmmanuelOchaje <114095241+EmmanuelOchaje@users.noreply.github.com>
2025-12-09 13:23:27 +0100

    commit

---
+++ src/app/components/input.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import {
+  ArrowLeftRight,
+  ChevronLeft,
+  ChevronRight,
+  Search,
+} from "lucide-react";
+
+const Input = ({ trip }) => {
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row gap-3 mb-2">
+        <div className="p-2 md:p-3 rounded-xl hover:border-black cursor-pointer border-gray-100 border relative flex-1">
+          <label className="text-xs text-gray-500 mb-1 block">From</label>
+          <input
+            type="text"
+            placeholder="Enter origin"
+            className="w-ful px-2 py-1 border-gray-200 rounded-lg focus:outline-none"
+          />
+        </div>
+        {/*swap btn, hidden on mobilee */}
+        <div className="hidden md:flex items-end justify-center pb-5">
+          <button className="p-2 cursor-pointer border border-gray-100 rounded-full">
+            <ArrowLeftRight className="w-5 h-5 text-gray-400" />
+          </button>
+        </div>
+        {/* to */}
+        <div className="border rounded-xl hover:border-black cursor-pointer border-gray-200 p-2 md:p-3 flex-1">
+          <label className="text-xs text-gray-500 mb-1 block">To</label>
+          <input
+            type="text"
+            placeholder="Enter destination"
+            className="w-full px-2 py-1 rounded-lg focus:outline-none"
+          />
+        </div>
+        {/* depart date */}
+        <div className="border p-2 md:p-3 hover:border-black cursor-pointer rounded-xl border-gray-200 flex-1">
+          <label className="text-xs text-gray-500 mb-1 block">Depart</label>
+          <div className="flex justify-between px-3 gap-1">
+            <p className="py-1 text-center text-sm rounded-lg focus:outline-none">
+              Sat, 06 Dec 2025
+            </p>
+            <div className="flex gap-4">
+              <button className="p-1 rounded">
+                <ChevronLeft className="w-4 h-4" />
+              </button>
+              <button className="p-1 rounded">
+                <ChevronRight className="w-4 h-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* return date */}
+        <div
+          className={`border ${
+            trip === "round-trip" ? "block" : "hidden"
+          } p-3 rounded-xl hover:border-black cursor-pointer border-gray-200 flex-1`}
+        >
+          <label className="text-xs text-gray-500 mb-1 block">Return</label>
+          <input
+            type="text"
+            placeholder="Select return date"
+            className="w-full px-2 py-1 text-sm rounded-lg focus:outline-none"
+          />
+        </div>
+      </div>
+      <div
+        className={`${
+          trip === "multi-city" ? "block" : "hidden"
+        } flex flex-col md:flex-row gap-3 mb-2`}
+      >
+        {/* from */}
+        <div className="p-2 md:p-3 rounded-xl hover:border-black cursor-pointer border-gray-100 border relative flex-1">
+          <label className="text-xs text-gray-500 mb-1 block">From</label>
+          <input
+            type="text"
+            placeholder="Enter origin"
+            className="w-ful px-2 py-1 border-gray-200 rounded-lg focus:outline-none"
+          />
+        </div>
+
+        {/*swap btn, hidden on mobilee */}
+        <div className="hidden md:flex items-end justify-center pb-5">
+          <button className="p-2 cursor-pointer border border-gray-100 rounded-full">
+            <ArrowLeftRight className="w-5 h-5 text-gray-400" />
+          </button>
+        </div>
+
+        {/* to */}
+        <div className="border rounded-xl hover:border-black cursor-pointer border-gray-200 p-2 md:p-3 flex-1">
+          <label className="text-xs text-gray-500 mb-1 block">To</label>
+          <input
+            type="text"
+            placeholder="Enter destination"
+            className="w-full px-2 py-1 rounded-lg focus:outline-none"
+          />
+        </div>
+
+        {/* depart date */}
+        <div className="border p-2 md:p-3 hover:border-black cursor-pointer rounded-xl border-gray-200 flex-1">
+          <label className="text-xs text-gray-500 mb-1 block">Depart</label>
+          <div className="flex justify-between px-3 gap-1">
+            <p className="py-1 text-center text-sm rounded-lg focus:outline-none">
+              Sat, 06 Dec 2025
+            </p>
+            <div className="flex">
+              <button className="p-1 rounded">
+                <ChevronLeft className="w-4 h-4 text-[#56cc1c]" />
+              </button>
+              <button className="p-1 rounded">
+                <ChevronRight className="w-4 h-4 text-[#56cc1c]" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* return date */}
+        <div
+          className={`border ${
+            trip === "round-trip" ? "block" : "hidden"
+          } p-3 rounded-xl hover:border-black cursor-pointer border-gray-200 flex-1`}
+        >
+          <label className="text-xs text-gray-500 mb-1 block">Return</label>
+          <input
+            type="text"
+            placeholder="Select return date"
+            className="w-full px-2 py-1 text-sm rounded-lg focus:outline-none"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
+{
+  /* from */
+}
--- src/app/components/tripSelector.jsx
+++ src/app/components/tripSelector.jsx
@@ -4,80 +4,51 @@ import {
   ChevronRight,
   Search,
 } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
+import Input from "./input";
 
 const TripSelector = () => {
+  const [trip, setTrip] = useState("one-way");
   return (
     <div className="bg-white p-4 mx-auto md:p-8 lg:p-10 pb-5 w-full md:w-fit md:rounded-xl">
       {/* trip type */}
       <div className="flex gap-1 mb-4 overflow-x-auto">
-        <button className="px-3 md:px-6 text-xs md:text-normal py-2 rounded-full transition-colors whitespace-nowrap text-gray-600 bg-gray-100">
+        <button
+          className={`px-3 md:px-6 text-xs md:text-normal font-semibold py-2 rounded-full transition-colors whitespace-nowrap ${
+            trip === "one-way"
+              ? "bg-[#e7fddc] text-[#238f2a]"
+              : "text-gray-600 bg-gray-100"
+          } cursor-pointer`}
+          onClick={() => setTrip("one-way")}
+        >
           One-way
         </button>
-        <button className="px-3 md:px-6 text-xs md:text-normal py-2 rounded-full transition-colors whitespace-nowrap bg-lime-100 text-emerald-700">
+        <button
+          className={`px-3 md:px-6 text-xs md:text-normal font-semibold py-2 rounded-full transition-colors whitespace-nowrap ${
+            trip === "round-trip"
+              ? "bg-[#e7fddc] text-[#238f2a]"
+              : "bg-gray-100 text-gray-600"
+          } cursor-pointer`}
+          onClick={() => setTrip("round-trip")}
+        >
           Round-trip
         </button>
-        <button className="px-3 md:px-6 text-xs md:text-normal py-2 rounded-full transition-colors whitespace-nowrap text-gray-600 bg-gray-100">
+        <button
+          className={`px-3 md:px-6 text-xs md:text-normal font-semibold py-2 rounded-full transition-colors whitespace-nowrap text-gray-600 cursor-pointer ${
+            trip === "multi-city"
+              ? "bg-[#e7fddc] text-[#238f2a]"
+              : "bg-gray-100 text-gray-600"
+          }`}
+          onClick={() => setTrip("multi-city")}
+        >
           Multi-city
         </button>
       </div>
 
       {/* search inputs */}
-      <div className="flex flex-col md:flex-row gap-3 mb-2">
-        {/* from */}
-        <div className="p-2 md:p-3 rounded-xl border-gray-100 border relative flex-1">
-          <label className="text-xs text-gray-500 mb-1 block">From</label>
-          <input
-            type="text"
-            placeholder="Enter origin"
-            className="w-ful px-2 py-1 border-gray-200 rounded-lg focus:outline-none"
-          />
-        </div>
-
-        {/*swap btn, hidden on mobilee */}
-        <div className="hidden md:flex items-end justify-center pb-5">
-          <button className="p-2 border border-gray-100 rounded-full">
-            <ArrowLeftRight className="w-5 h-5 text-gray-400" />
-          </button>
-        </div>
-
-        {/* to */}
-        <div className="border rounded-xl border-gray-200 p-2 md:p-3 flex-1">
-          <label className="text-xs text-gray-500 mb-1 block">To</label>
-          <input
-            type="text"
-            placeholder="Enter destination"
-            className="w-full px-2 py-1 rounded-lg focus:outline-none"
-          />
-        </div>
-
-        {/* depart date */}
-        <div className="border p-2 md:p-3 rounded-xl border-gray-200 flex-1">
-          <label className="text-xs text-gray-500 mb-1 block">Depart</label>
-          <div className="flex justify-between px-3 gap-1">
-            <p className="py-1 text-center text-sm rounded-lg focus:outline-none">
-              Sat, 06 Dec 2025
-            </p>
-            <div className="flex gap-4">
-              <button className="p-1 rounded">
-                <ChevronLeft className="w-4 h-4" />
-              </button>
-              <button className="p-1 rounded">
-                <ChevronRight className="w-4 h-4" />
-              </button>
-            </div>
-          </div>
-        </div>
-
-        {/* return date */}
-        <div className="border p-3 rounded-xl border-gray-200 flex-1">
-          <label className="text-xs text-gray-500 mb-1 block">Return</label>
-          <input
-            type="text"
-            placeholder="Select return date"
-            className="w-full px-2 py-1 text-sm rounded-lg focus:outline-none"
-          />
-        </div>
+      <div className="flex flex-col">
+        <Input trip={trip} />
+        {/* multi city extra */}
       </div>
 
       {/* options */}
